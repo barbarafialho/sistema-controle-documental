9f497b71206592893c3f172843d5fa3536870cbc
Barbara <barbarafialho@aluno.ifsp.edu.br>
2026-04-29 19:28:58 -0300

    config: remove application-local.yaml

--- src/main/java/br/com/barbara/sistema_controle_documental/controller/AdminUsuarioController.java
+++ src/main/java/br/com/barbara/sistema_controle_documental/controller/AdminUsuarioController.java
@@ -1,11 +1,14 @@
 package br.com.barbara.sistema_controle_documental.controller;
 
+import br.com.barbara.sistema_controle_documental.dto.PerfilDTO;
 import br.com.barbara.sistema_controle_documental.dto.UsuarioRequestDTO;
 import br.com.barbara.sistema_controle_documental.dto.UsuarioResponseDTO;
+import br.com.barbara.sistema_controle_documental.model.Usuario;
 import br.com.barbara.sistema_controle_documental.service.UsuarioService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/usuarios")
 @Tag(name = "Usuário", description = "Endpoint para gerenciamento de usuários")
-public class UsuarioController{
+public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
@@ -23,27 +26,28 @@ public class UsuarioController{
 
     @GetMapping
     public ResponseEntity<List<UsuarioResponseDTO>> listarTodos() {
-        return ResponseEntity.ok(usuarioService.findAll());
+        return ResponseEntity.ok(usuarioService.listarTodos());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDTO> listarPorId(@PathVariable("id") Long id){
-        return ResponseEntity.ok(usuarioService.findById(id));
+        return ResponseEntity.ok(usuarioService.listarPorId(id));
     }
 
     @PostMapping
     public ResponseEntity<UsuarioResponseDTO> criar(@RequestBody UsuarioRequestDTO dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.save(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.salvar(dto));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<UsuarioResponseDTO> atualizar(@PathVariable Long id, @RequestBody UsuarioRequestDTO dto) {
-        return ResponseEntity.ok(usuarioService.update(id, dto));
+    @PutMapping("/perfil")
+    public ResponseEntity<UsuarioResponseDTO> atualizarMeusDados(@RequestBody PerfilDTO dto) {
+        Usuario logado = (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok(usuarioService.atualizarPerfil(dto, logado));
     }
 
-    @DeleteMapping("/{id}")
+    @PutMapping("/{id}/desativar")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
-        usuarioService.delete(id);
+        usuarioService.desativar(id);
         return ResponseEntity.noContent().build();
     }
 }

107cd7f5fc27f51681e46fcb4d4b939bf516a5cf
Barbara <barbarafialho@aluno.ifsp.edu.br>
2026-05-16 19:22:16 -0300

    feat: implementa upload e download de arquivos

---
+++ src/main/java/br/com/barbara/sistema_controle_documental/exceptions/FileStorageException.java
@@ -0,0 +1,7 @@
+package br.com.barbara.sistema_controle_documental.exceptions;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+}
